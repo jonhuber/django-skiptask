c4386847d53e942cf85ec8ffe8b3a7a94af2811b
Jon Huber <jon@jonhuber.com>
2025-07-06 11:33:05 -0500

    Start a new Django app for a database based task queue

---
+++ tests/settings.py
@@ -0,0 +1,80 @@
+from pathlib import Path
+import os
+
+BASE_DIR = Path(__file__).resolve().parent
+
+SECRET_KEY = 'NOT_USED'
+
+DEBUG = True
+
+ALLOWED_HOSTS = []
+
+INSTALLED_APPS = [
+    'django_skiptask',
+]
+
+MIDDLEWARE = []
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+            ],
+        },
+    },
+]
+
+WSGI_APPLICATION = 'task.wsgi.application'
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
+
+LANGUAGE_CODE = 'en-us'
+
+TIME_ZONE = 'UTC'
+
+USE_I18N = True
+
+USE_L10N = True
+
+USE_TZ = True
+
+STATIC_URL = '/static/'
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'root': {
+        'handlers': ['console'],
+        'level': 'INFO',
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console'],
+            'level': os.getenv('DJANGO_LOG_LEVEL', 'INFO'),
+            'propagate': False,
+        },
+        'django_skiptask': {
+            'handlers': ['console'],
+            'level': os.getenv('DJANGO_LOG_LEVEL', 'INFO'),
+            'propagate': False,
+        },
+        'django.db.backends': {
+            'handlers': ['console'],
+            'level': os.getenv('DJANGO_DB_LOG_LEVEL', 'INFO'),
+            'propagate': False,
+        }
+    },
+}
